ab896c383b179d78ba2784b92d796ed12bda7ac5
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-03 18:58:49 -0300

    feat: added class-validator

--- src/modules/destiny/dto/create-destiny.dto.ts
+++ src/modules/destiny/dto/create-destiny.dto.ts
@@ -1,9 +1,11 @@
 import { Prisma } from "@prisma/client";
+import { Destiny } from "../entities/destiny.entity";
 
-export class CreateDestinyDto implements Prisma.destiniesCreateInput {
+export class CreateDestinyDto implements Destiny {
+    id?: number;
     state: string;
     bestSeason: string;
     weather: string;
     imageUrl: string;
-    touristics?: Prisma.touristicsCreateNestedManyWithoutDestiniesInput;;
+    touristics?: Prisma.touristicsUncheckedCreateNestedManyWithoutDestiniesInput;
 }
--- src/modules/destiny/dto/update-destiny.dto.ts
+++ src/modules/destiny/dto/update-destiny.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDestinyDto } from './create-destiny.dto';
-import { Prisma } from '@prisma/client';
 
 export class UpdateDestinyDto extends PartialType(CreateDestinyDto) {}
