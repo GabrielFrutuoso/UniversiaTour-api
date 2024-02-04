a8d41aeb0d2bb6774f3b4a140f3d048d60b1a4f8
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-04 15:02:37 -0300

    feat: adding user resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DestinyModule } from './modules/destiny/destiny.module';
 import { TouristicsModule } from './modules/touristics/touristics.module';
+import { UserModule } from './modules/user/user.module';
 
 
 @Module({
-  imports: [DestinyModule, TouristicsModule],
+  imports: [DestinyModule, TouristicsModule, UserModule],
   controllers: [],
   providers: [],
 })
