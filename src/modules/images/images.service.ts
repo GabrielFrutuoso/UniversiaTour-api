30cc561a9ee96f7f38a17fe7043985851c3aedf7
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-04 17:25:20 -0300

    feat: adding PrismaService in imagesService

--- src/modules/images/images.service.ts
+++ src/modules/images/images.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ImagesService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createImageDto: CreateImageDto) {
     return 'This action adds a new image';
   }
 
   findAll() {
-    return `This action returns all images`;
+    return this.prisma.images.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} image`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} image`;
+  // }
 
   update(id: number, updateImageDto: UpdateImageDto) {
     return `This action updates a #${id} image`;
