55d5cb8d1492ac7f79ee8d3980709f34a993102f
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-04 15:10:49 -0300

    refator: setting up user dto´s

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { User } from "../entities/user.entity";
+
+export class CreateUserDto implements User {
+    id: `${string}-${string}-${string}-${string}-${string}`;
+    username: string;
+    email: string;
+    password: string;
+    sex?: string;
+    bio?: string;
+    avatar?: string;
+    birthday?: Date;
+    tripInterests?: string;
+    role: "USER" | "ADMIN";
+}
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+import { UUID } from "crypto";
+
+export class User {
+    id: UUID;
+    username: string;
+    email: string;
+    password: string;
+    sex?: string;
+    bio?: string;
+    avatar?: string;
+    birthday?: Date;
+    tripInterests?: string;
+    role: "USER" | "ADMIN";
+}
