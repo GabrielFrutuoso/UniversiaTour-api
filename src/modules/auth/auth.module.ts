657d875e6841e95641be8f5af651fe5d240dbf82
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-05 17:19:13 -0300

    feat: add local auth guard and login endpoint

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UserModule, 
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
