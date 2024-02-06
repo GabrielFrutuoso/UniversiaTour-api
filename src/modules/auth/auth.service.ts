ed954a4b26d984ff404668bf988a559cd4a47a9c
GabrielFrutuoso <mackbiel03@gmail.com>
2024-02-06 14:36:08 -0300

    feat: implements the access token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {} 
+  constructor(
+    private readonly userService: UserService, 
+    private readonly jwtService: JwtService
+    ) {} 
+
+  login(user) {
+    const payload = { 
+      email: user.email, 
+      sub: user.id, 
+      username: user.username 
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
   async validateUser(email: string, password: string) {
       const user = await this.userService.findByEmail(email);
 
@@ -19,7 +36,5 @@ export class AuthService {
         throw new Error('Email adress or password is incorrect');
       }
   }
-  login() {
-    return 'logado';
-  }
+  
 }
