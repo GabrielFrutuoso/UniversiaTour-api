8d467bfcd6a119f2b1ea082c28410c62d8c40bc3
GabrielFrutuoso <mackbiel03@gmail.com>
2024-03-03 16:01:54 -0300

    fix: fixing

--- src/modules/destiny/dto/create-destiny.dto.ts
+++ src/modules/destiny/dto/create-destiny.dto.ts
@@ -2,30 +2,14 @@ import { Prisma } from "@prisma/client";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateDestinyDto implements Prisma.destiniesUncheckedCreateInput {
-    @IsNotEmpty()
-    @IsString()
+    id?: number;
     state: string;
-      
-    @IsNotEmpty()
-    @IsString()
     best_season: string;
-
-    @IsNotEmpty()
-    @IsString()
     weather: string;
-
-    @IsNotEmpty()
-    @IsString()
-    image_url: string;
-
-    @IsNotEmpty()
-    @IsString()
     local_maps: string;
-
-    @IsNotEmpty()
-    @IsString()
     description: string;
-
-    @IsOptional()
+    image_url: string;
+    
     touristics?: Prisma.touristicsUncheckedCreateNestedManyWithoutDestiniesInput;
+
 }
