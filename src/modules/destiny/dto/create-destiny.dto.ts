5c2fbd49ee0858bf100e173c5838da17816702df
GabrielFrutuoso <mackbiel03@gmail.com>
2024-03-04 12:11:02 -0300

    stiil trying to fix create-destiny type

--- src/modules/destiny/dto/create-destiny.dto.ts
+++ src/modules/destiny/dto/create-destiny.dto.ts
@@ -1,14 +1,13 @@
 import { Prisma } from "@prisma/client";
-import { Destiny } from "../entities/destiny.entity";
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class CreateDestinyDto implements Prisma.destiniesUncheckedCreateInput {
+
+export class CreateDestinyDto {
     state: string;
     best_season: string;
     weather: string;
     local_maps: string;
     description: string;
     image_url: string;
+    touristic_id: number
     touristics?: Prisma.touristicsCreateNestedManyWithoutDestiniesInput;
-
 }
