fbafdd5b214b1b474f9ef0ceef47767f8a93f7ab
GabrielFrutuoso <mackbiel03@gmail.com>
2024-03-04 12:03:45 -0300

    fix: changed create-destiny type

--- src/modules/destiny/dto/create-destiny.dto.ts
+++ src/modules/destiny/dto/create-destiny.dto.ts
@@ -2,31 +2,13 @@ import { Prisma } from "@prisma/client";
 import { Destiny } from "../entities/destiny.entity";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class CreateDestinyDto implements Destiny {
-    @IsNotEmpty()
-    @IsString()
+export class CreateDestinyDto implements Prisma.destiniesCreateInput {
     state: string;
-      
-    @IsNotEmpty()
-    @IsString()
     best_season: string;
-
-    @IsNotEmpty()
-    @IsString()
     weather: string;
-
-    @IsNotEmpty()
-    @IsString()
-    image_url: string;
-
-    @IsNotEmpty()
-    @IsString()
     local_maps: string;
-
-    @IsNotEmpty()
-    @IsString()
     description: string;
+    image_url: string;
+    touristics?: Prisma.touristicsCreateNestedManyWithoutDestiniesInput;
 
-    @IsOptional()
-    touristics?: Prisma.touristicsUncheckedCreateNestedManyWithoutDestiniesInput;
 }
